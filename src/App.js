620effbfddc98f302d9dda852706afa342134b4f
Rocioegrajales <rocioegrajales@gmail.com>
2021-03-26 18:42:02 -0500

    ahora está el contenido en español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React application - This is my application</h1>       
+        <h1>Aplicación de React - Esta es mi aplicación</h1>       
         <a
           className="App-link"
           href="https://reactjs.org"
